f5b63c56357d5fee2f6314470bb73c2b3e83da15
Shawn Rodgers <shawnrodgers266@gmail.com>
2021-12-23 13:58:27 -0500

    finished class documentation

--- include/TextBox.h
+++ include/TextBox.h
@@ -14,20 +14,79 @@ protected:
 
 public:
 
-
+    /**
+     * Initializes text box with text as "Default text"
+     */
     TextBox();
+
+    /**
+     * Initializes text box with custom text
+     *
+     * @param text custom text
+     */
     explicit TextBox(std::string text);
+
+    /**
+     * Initializes text box with custom text and custom color
+     *
+     * @param text custom text
+     * @param stringColor text color
+     */
     TextBox(std::string text, color stringColor);
+
+    /**
+     * Initializes text box with custom text, color, center, fill
+     *
+     * @param text custom text
+     * @param stringColor text color
+     * @param center center point of box
+     * @param fill background color
+     */
     TextBox(std::string text, color stringColor, point center, color fill);
 
     virtual ~TextBox() = default;
 
-    void toggleButton();
+    /**
+     * Set text of textbox
+     *
+     * @param text text
+     */
     void setText(std::string text);
+
+    /**
+     * Get text of textbox
+     *
+     * @return text
+     */
     std::string getText() const;
+
+    /**
+     * The only way to center text when bit mapping is to find the right
+     * offset value, often it is between 4.0 and 5.5 for short strings
+     *
+     * @param offset offset value for mapping text
+     */
     void setOffset(double offset);
+
+    /**
+     * The only way to center text when bit mapping is to find the right
+     * offset value, often it is between 4.0 and 5.5 for short strings
+     *
+     * @return offset value for mapping text
+     */
     double getOffset() const;
+
+    /**
+     * Moves text box and text
+     *
+     * @param deltaX change in x value
+     * @param deltaY change in y value
+     */
     void move(int deltaX, int deltaY);
+
+    /**
+     * Draw text box and text to display
+     */
     void draw() const override;
 
 };
